2017384ed3ca040eac5e604d26321765e8a5e7ae
Daniel van Dorp <daniel.van.dorp@mendix.com>
2016-03-02 10:02:22 +0100

    better test naming

--- tests/usecase/test_check_app_deployed_unauthorized.py
+++ tests/usecase/test_check_app_deployed_unauthorized.py
@@ -3,8 +3,8 @@ import requests
 import os
 
 
-class CheckTestAppDeployedTest(unittest.TestCase):
-    def test_app_deployed(self):
+class TestCaseAppDeployed(unittest.TestCase):
+    def test_app_deployed_unauthorized(self):
         # assumes the app route is coming from env var
         app_name = os.environ.get("APP_NAME")
         full_uri = "https://" + app_name + "." + os.environ.get("CF_DOMAIN") + "/xas/"
--- tests/usecase/test_check_mxadmin_exposed_authorized.py
+++ tests/usecase/test_check_mxadmin_exposed_authorized.py
@@ -3,8 +3,17 @@ import requests
 import os
 
 
-class CheckTestMxAdminExposedAuthorizedTest(unittest.TestCase):
-    def test_app_deployed(self):
+class TestCaseMxAdminExposed(unittest.TestCase):
+    def test_mxadmin_exposed_unauthorized(self):
+        # assumes the app route is coming from env var
+        app_name = os.environ.get("APP_NAME")
+        full_uri = "https://" + app_name + "." + os.environ.get("CF_DOMAIN") + "/_mxadmin/"
+
+        r = requests.get(full_uri)
+        assert r.status_code == 401
+
+
+    def test_mxadmin_exposed_authorized(self):
         # assumes the app route is coming from env var
         app_name = os.environ.get("APP_NAME")
         full_uri = "https://" + app_name + "." + os.environ.get("CF_DOMAIN") + "/_mxadmin/"
