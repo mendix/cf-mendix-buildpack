57ca0f04153774549d81eddfc03b6c97493b92dd
Jouke Waleson <jouke.waleson@mendix.com>
2016-08-29 17:23:11 +0200

    lower parallel factor of tests to prevent errors
    sometimes these errors would pop up when starting 6+ apps at the same
    time:
    
    Starting app ops-aa94a6dc.cf-test.mendix.com [........]
    FAILED
    Server error, status code: 400, error code: 170001, message: Staging
    error: failed to stage application:
    Not enough disk resources available
    
    We should probably bump
    
      disk_mb: 2048
      memory_mb: 1024
      staging_disk_limit_mb: 4096
      staging_memory_limit_mb: 1024
    
    in dea_next config

--- tests/run.sh
+++ tests/run.sh
@@ -23,4 +23,4 @@ virtualenv -p python2 venv
 pip install -r requirements.txt
 
 
-python venv/bin/nosetests -vv --processes=10 --process-timeout=600 usecase/
+python venv/bin/nosetests -vv --processes=4 --process-timeout=600 usecase/
