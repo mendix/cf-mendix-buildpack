7c71e55b3f6697828e3927a649d14ff0b928056d
Jouke Waleson <jouke.waleson@mendix.com>
2016-08-29 16:13:09 +0200

    add test for sampledata

---
+++ tests/usecase/test_sampledata.py
@@ -0,0 +1,27 @@
+import basetest
+import subprocess
+import requests
+import json
+
+
+class TestCaseSampleData(basetest.BaseTest):
+
+    def setUp(self):
+        self.setUpCF('MontBlancApp671WithSampleData.mpk')
+        subprocess.check_call(('cf', 'start', self.app_name))
+
+    def test_user_from_sampledata_can_log_in(self):
+        full_uri = "https://" + self.app_name + "/xas/"
+        login_action = {
+            'action': 'login',
+            'params': {
+                'username': 'henk',
+                'password': 'henkie',
+            },
+        }
+        r = requests.post(
+            full_uri,
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(login_action),
+        )
+        assert r.status_code == 200
