e0a1f1f01a2328c40b7dede641935edab9df39c6
Riccardo M. Cefala <riccardo.cefala@mendix.com>
2016-09-16 13:15:55 +0200

    prevent emitter thread from exiting on exceptions

--- lib/metrics.py
+++ lib/metrics.py
@@ -14,17 +14,20 @@ class MetricsEmitterThread(threading.Thread):
     def run(self):
         logger.debug('Starting metrics emitter with interval %d' % self.interval)
         while True:
-            m2ee_stats, java_version = munin.get_stats_from_runtime(self.m2ee.client, self.m2ee.config)
-            m2ee_stats = munin.augment_and_fix_stats(
-                m2ee_stats,
-                self.m2ee.runner.get_pid(),
-                java_version)
-            critical_logs_count = len(self.m2ee.client.get_critical_log_messages())
-            m2ee_stats['critical_logs_count'] = critical_logs_count
-            stats = {
-                'version': '1.0',
-                'timestamp': datetime.datetime.now().isoformat(),
-                'mendix_runtime': m2ee_stats,
-            }
-            logger.info('MENDIX-METRICS: ' + json.dumps(stats))
+            try:
+                m2ee_stats, java_version = munin.get_stats_from_runtime(self.m2ee.client, self.m2ee.config)
+                m2ee_stats = munin.augment_and_fix_stats(
+                    m2ee_stats,
+                    self.m2ee.runner.get_pid(),
+                    java_version)
+                critical_logs_count = len(self.m2ee.client.get_critical_log_messages())
+                m2ee_stats['critical_logs_count'] = critical_logs_count
+                stats = {
+                    'version': '1.0',
+                    'timestamp': datetime.datetime.now().isoformat(),
+                    'mendix_runtime': m2ee_stats,
+                }
+                logger.info('MENDIX-METRICS: ' + json.dumps(stats))
+            except Exception as e:
+                logger.warn('Failed to emit Mendix runtime metrics: ' + str(e))
             time.sleep(self.interval)
