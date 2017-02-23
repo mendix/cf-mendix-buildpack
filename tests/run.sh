387458f70aa53f8c5b20c4f21ea3f69ce36389e8
Xiwen Cheng <xiwen.cheng@mendix.com>
2017-02-23 16:29:55 +0100

    tests: fixes xargs: cf: terminated by signal 13

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -e
 
 ls usecase/*.py
 
@@ -12,19 +11,20 @@ else
     exit 1
 fi
 
-cf login -a "$CF_ENDPOINT" -u "$CF_USER" -p "$CF_PASSWORD" -o "$CF_ORG" -s "$CF_SPACE"
+cf login -a "$CF_ENDPOINT" -u "$CF_USER" -p "$CF_PASSWORD" -o "$CF_ORG" -s "$CF_SPACE" || exit 1
+
+echo "Begin clean up of environment"
+cf apps | grep ops- | awk '{print $1}' | xargs -n 1 cf delete -r -f
+cf s | grep ops- | awk '{print $1}' | xargs -n 1 cf ds -f
+echo "Completed environment clean up"
 
 # cf login command above exposes the vars if set -x is on top.
+set -e
 set -x
 
 [ -d "venv" ] && rm -rf "venv"
 virtualenv -p python2 venv
 . venv/bin/activate
 pip install -r requirements.txt
-
-echo "Begin clean up of environment"
-cf apps | grep ops- | awk '{print $1}' | xargs -n 1 cf delete -r -f | true
-cf s | grep ops- | awk '{print $1}' | xargs -n 1 cf ds -f | true
-echo "Completed environment clean up"
 
 python venv/bin/nosetests -vv --processes=3 --process-timeout=600 usecase/
