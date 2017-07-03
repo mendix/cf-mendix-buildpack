76d188a1901d276b964911275f7f1ba7bcb539d5
Jouke Waleson <jouke.waleson@mendix.com>
2017-07-03 23:10:45 +0200

    more resilient old app deletion

--- tests/run.sh
+++ tests/run.sh
@@ -13,8 +13,8 @@ fi
 cf login -a "$CF_ENDPOINT" -u "$CF_USER" -p "$CF_PASSWORD" -o "$CF_ORG" -s "$CF_SPACE" > /dev/null
 
 echo "begin clean up of environment"
-cf apps 2>&1 | grep ops- | awk '{print $1}' | xargs -n 1 -P 5 --no-run-if-empty cf delete -r -f | grep -v 'OK'
-cf s 2>&1 | grep ops- | awk '{print $1}' | xargs -n 1 -P 5 --no-run-if-empty cf ds -f $service | grep -v 'OK'
+cf apps 2>&1 | grep ops- | awk '{print $1}' | xargs -n 1 -P 5 --no-run-if-empty cf delete -r -f | grep -v 'OK' || true
+cf s 2>&1 | grep ops- | awk '{print $1}' | xargs -n 1 -P 5 --no-run-if-empty cf ds -f $service | grep -v 'OK' || true
 echo "completed environment clean up"
 
 
