5ac790fc05d852e2f1df96fe07d4cab222bf2202
Jouke Waleson <jouke.waleson@mendix.com>
2017-07-03 16:37:50 +0200

    bump version

--- start.py
+++ start.py
@@ -23,7 +23,7 @@ import traceback
 logger.setLevel(buildpackutil.get_buildpack_loglevel())
 
 
-logger.info('Started Mendix Cloud Foundry Buildpack v1.4.18')
+logger.info('Started Mendix Cloud Foundry Buildpack v1.5.0')
 
 logging.getLogger('m2ee').propagate = False
 
