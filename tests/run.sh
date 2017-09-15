e3dd69a23d09516f4c143875d92e0e5ccfd7cfd7
Jouke Waleson <jouke.waleson@mendix.com>
2017-09-15 14:12:52 +0200

    also cleanup after each CI run, so resources are not left behind

--- tests/run.sh
+++ tests/run.sh
@@ -12,11 +12,17 @@ fi
 
 cf login -a "$CF_ENDPOINT" -u "$CF_USER" -p "$CF_PASSWORD" -o "$CF_ORG" -s "$CF_SPACE" > /dev/null
 
-echo "begin clean up of environment"
-cf apps 2>&1 | grep ops- | awk '{print $1}' | xargs -n 1 -P 5 --no-run-if-empty cf delete -r -f | grep -v 'OK' || true
-cf s 2>&1 | grep ops- | awk '{print $1}' | xargs -n 1 -P 5 --no-run-if-empty cf ds -f $service | grep -v 'OK' || true
-echo "completed environment clean up"
+function cleanup {
+    echo "begin clean up of environment"
+    cf apps 2>&1 | grep ops- | awk '{print $1}' | xargs -n 1 -P 5 --no-run-if-empty cf delete -r -f | grep -v 'OK' || true
+    cf s 2>&1 | grep ops- | awk '{print $1}' | xargs -n 1 -P 5 --no-run-if-empty cf ds -f $service | grep -v 'OK' || true
+    cf delete-orphaned-routes -f 2>&1 | grep -i deleting || true
+    echo "completed environment clean up"
+}
 
+cleanup
 
 echo 'starting test run, tests will run in parallel and output shown at the end'
 nosetests -vv --processes=10 --process-timeout=600 --with-timer usecase/
+
+cleanup
