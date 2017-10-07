6ba5e97f6c7c302b93197814e072e7b9782ad20a
Jouke Waleson <jouke.waleson@mendix.com>
2017-10-07 12:44:40 +0200

    bump version

--- start.py
+++ start.py
@@ -25,7 +25,7 @@ from buildpackutil import i_am_primary_instance
 logger.setLevel(buildpackutil.get_buildpack_loglevel())
 
 
-logger.info('Started Mendix Cloud Foundry Buildpack v1.6.3')
+logger.info('Started Mendix Cloud Foundry Buildpack v1.7.0')
 
 logging.getLogger('m2ee').propagate = False
 
