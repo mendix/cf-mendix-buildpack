04b30be19e919f99fcc0acffb70309cfe31c1257
Jouke Waleson <jouke.waleson@mendix.com>
2018-01-10 15:44:35 +0100

    log mendix runtime config on debug

--- lib/m2ee/core.py
+++ lib/m2ee/core.py
@@ -265,6 +265,7 @@ class M2EE():
                 config[option] = ','.join(config[option])
 
         logger.debug("Sending MxRuntime configuration...")
+        logger.debug(str(config))
         m2eeresponse = self.client.update_configuration(config)
         result = m2eeresponse.get_result()
         if result == 1:
