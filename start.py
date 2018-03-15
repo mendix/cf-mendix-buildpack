f8b76aefb96a49574692c97465aff16d53ed3cde
Jouke Waleson <jouke.waleson@mendix.com>
2018-03-15 14:18:40 +0100

    v1.9.0: mysql auto-wiring, fix test, show better errors

--- start.py
+++ start.py
@@ -24,7 +24,7 @@ from buildpackutil import i_am_primary_instance
 logger.setLevel(buildpackutil.get_buildpack_loglevel())
 
 
-logger.info('Started Mendix Cloud Foundry Buildpack v1.8.7')
+logger.info('Started Mendix Cloud Foundry Buildpack v1.9.0')
 
 logging.getLogger('m2ee').propagate = False
 
