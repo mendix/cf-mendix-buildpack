8d5790716d8edd893912712e54b63843b206d13e
Jouke Waleson <jouke.waleson@mendix.com>
2018-04-16 17:32:31 +0200

    v1.9.2: add datadog and bump log / jetty limits

--- start.py
+++ start.py
@@ -26,7 +26,7 @@ from buildpackutil import i_am_primary_instance
 logger.setLevel(buildpackutil.get_buildpack_loglevel())
 
 
-logger.info('Started Mendix Cloud Foundry Buildpack v1.9.1')
+logger.info('Started Mendix Cloud Foundry Buildpack v1.9.2')
 
 logging.getLogger('m2ee').propagate = False
 
