e57178754f2b093f0fcbfdb8c7485ff704750be8
Daniel van Dorp <daniel.van.dorp@mendix.com>
2018-06-05 13:09:57 +0200

    v2.0.3 resolve incorrect heap size warning

--- start.py
+++ start.py
@@ -26,7 +26,7 @@ from buildpackutil import i_am_primary_instance  # noqa: E402
 logger.setLevel(buildpackutil.get_buildpack_loglevel())
 
 
-logger.info('Started Mendix Cloud Foundry Buildpack v2.0.2')
+logger.info('Started Mendix Cloud Foundry Buildpack v2.0.3')
 
 logging.getLogger('m2ee').propagate = False
 
