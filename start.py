22d0e8f3f82bfae98eb77324f97d0aa5ed9f9436
Josh Palmer <Josh.Palmer@mendix.com>
2018-10-25 13:58:33 +0200

    Bump buildpack version

--- start.py
+++ start.py
@@ -53,7 +53,7 @@ HEARTBEAT_STRING_LIST = codecs.encode(HEARTBEAT_SOURCE_STRING, "rot13").split(
 )
 
 logger.setLevel(buildpackutil.get_buildpack_loglevel())
-logger.info("Started Mendix Cloud Foundry Buildpack v2.2.2")
+logger.info("Started Mendix Cloud Foundry Buildpack v2.2.3")
 logging.getLogger("m2ee").propagate = False
 
 
