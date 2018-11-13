ca0ad25a13d5559401aeda318b0f6642852a4e8b
Josh Palmer <Josh.Palmer@mendix.com>
2018-11-13 10:51:00 +0100

    DEP-919 Change delete from storage to use newer setting.

--- start.py
+++ start.py
@@ -431,7 +431,7 @@ def _get_s3_specific_config(vcap_services, m2ee):
 
     if dont_perform_deletes:
         logger.debug("disabling perform deletes for runtime")
-        config["com.mendix.storage.s3.PerformDeleteFromStorage"] = False
+        config["com.mendix.storage.PerformDeleteFromStorage"] = False
     if key_suffix:
         config["com.mendix.storage.s3.ResourceNameSuffix"] = key_suffix
     if v2_auth:
