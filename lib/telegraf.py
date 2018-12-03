c598959f36732bfdea8c6380bf649c24135ea9e5
ClydeW <MXClyde@users.noreply.github.com>
2018-12-03 12:56:20 +0100

    Update telegraf.py

--- lib/telegraf.py
+++ lib/telegraf.py
@@ -197,16 +197,15 @@ def update_config(m2ee, app_name):
     # Expose metrics with Prometheus Client Serice when enabled
     if _get_appmetrics_prometheus is not None:
         _write_prometheus_output_config()
-        return
-    
-    # # Write http_oputs (one or array)
-    if _get_appmetrics_target is not None:
-        http_configs = json.loads(_get_appmetrics_target())
-        if type(http_configs) is list:
-            for http_config in http_configs:
-                _write_http_output_config(http_config)
-        else:
-            _write_http_output_config(http_configs)
+    else:
+        # # Write http_outputs (one or array)
+        if _get_appmetrics_target is not None:
+            http_configs = json.loads(_get_appmetrics_target())
+            if type(http_configs) is list:
+                for http_config in http_configs:
+                    _write_http_output_config(http_config)
+            else:
+                _write_http_output_config(http_configs)
 
 
 def compile(install_path, cache_dir):
