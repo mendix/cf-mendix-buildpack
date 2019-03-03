c518dd3e74c07a76b2addd8aa7cffba3dfeee67e
Pim van den Berg <pim.van.den.berg@mendix.com>
2019-03-03 12:14:02 +0100

    start.py: rm "The Zen of Python" from the logging heartbeat message
    Let's just stop this madness. It causes unnecessary support tickets.
    We have better things to do.

--- start.py
+++ start.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 import atexit
 import base64
-import codecs
 import datetime
-import itertools
 import json
 import logging
 import os
@@ -27,30 +25,6 @@ import requests  # noqa: E402
 from m2ee import M2EE, logger  # noqa: E402
 from nginx import get_path_config, gen_htpasswd  # noqa: E402
 from buildpackutil import i_am_primary_instance  # noqa: E402
-
-HEARTBEAT_SOURCE_STRING = """Gur Mra bs Clguba, ol Gvz Crgref
-Ornhgvshy vf orggre guna htyl.
-Rkcyvpvg vf orggre guna vzcyvpvg.
-Fvzcyr vf orggre guna pbzcyrk.
-Pbzcyrk vf orggre guna pbzcyvpngrq.
-Syng vf orggre guna arfgrq.
-Fcnefr vf orggre guna qrafr.
-Ernqnovyvgl pbhagf.
-Fcrpvny pnfrf nera'g fcrpvny rabhtu gb oernx gur ehyrf.
-Nygubhtu cenpgvpnyvgl orngf chevgl.
-Reebef fubhyq arire cnff fvyragyl.
-Hayrff rkcyvpvgyl fvyraprq.
-Va gur snpr bs nzovthvgl, ershfr gur grzcgngvba gb thrff.
-Gurer fubhyq or bar-- naq cersrenoyl bayl bar --boivbhf jnl gb qb vg.
-Nygubhtu gung jnl znl abg or boivbhf ng svefg hayrff lbh'er Qhgpu.
-Abj vf orggre guna arire.
-Nygubhtu arire vf bsgra orggre guna *evtug* abj.
-Vs gur vzcyrzragngvba vf uneq gb rkcynva, vg'f n onq vqrn.
-Vs gur vzcyrzragngvba vf rnfl gb rkcynva, vg znl or n tbbq vqrn.
-Anzrfcnprf ner bar ubaxvat terng vqrn -- yrg'f qb zber bs gubfr!"""
-HEARTBEAT_STRING_LIST = codecs.encode(HEARTBEAT_SOURCE_STRING, "rot13").split(
-    "\n"
-)
 
 logger.setLevel(buildpackutil.get_buildpack_loglevel())
 
@@ -1195,8 +1169,8 @@ class LoggingHeartbeatEmitterThread(threading.Thread):
         logger.debug(
             "Starting metrics emitter with interval %d", self.interval
         )
-        for line in itertools.cycle(HEARTBEAT_STRING_LIST):
-            logger.info("MENDIX-LOGGING-HEARTBEAT: %s", line)
+        while True:
+            logger.info("MENDIX-LOGGING-HEARTBEAT: .")
             time.sleep(self.interval)
 
 
