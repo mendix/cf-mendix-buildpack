7df4233a627af909fcbb77e380474c69e6a9fc2c
Chris Wesdorp <chris.wesdorp@mendix.com>
2019-04-13 00:08:27 +0200

    Wait for mxbuild to be ready for serving requests

--- tests/usecase/test_fastdeploy.py
+++ tests/usecase/test_fastdeploy.py
@@ -7,8 +7,11 @@ class TestCaseFastdeploy(basetest.BaseTest):
     def setUp(self):
         super().setUp()
         self.setUpCF(
-            "MontBlancApp720.mpk",
-            env_vars={"DEPLOY_PASSWORD": self.mx_password},
+            "ci-buildpack-test-app-mx7-18-3.mpk",
+            env_vars={
+                "DEPLOY_PASSWORD": self.mx_password,
+                "USE_DATA_SNAPSHOT": "false",
+            },
         )
         self.startApp()
 
@@ -18,15 +21,24 @@ class TestCaseFastdeploy(basetest.BaseTest):
                 "wget",
                 "--quiet",
                 "https://s3-eu-west-1.amazonaws.com"
-                "/mx-buildpack-ci/MontBlancApp720b.mpk",
+                "/mx-buildpack-ci/ci-buildpack-test-app-mx7-18-3.mpk",
             )
         )
         full_uri = "https://" + self.app_name + "/_mxbuild/"
-        time.sleep(10)
+
+        max = 12
+        for attempt in range(0, max):
+            time.sleep(10)
+            r = requests.get(full_uri, auth=("deploy", self.mx_password))
+            if r.status_code == 501:
+                break
+            if attempt == max - 1:
+                raise Exception("Starting MxBuild takes too long")
+
         r = requests.post(
             full_uri,
             auth=("deploy", self.mx_password),
-            files={"file": open("MontBlancApp720b.mpk", "rb")},
+            files={"file": open("ci-buildpack-test-app-mx7-18-3.mpk", "rb")},
         )
 
         if r.status_code != 200:
