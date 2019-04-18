b48b65a08e58a6d0ee0bf34cab528ba42e40476f
ClydeW <MXClyde@users.noreply.github.com>
2019-04-19 01:19:32 +0200

    Update buildpackutil.py

--- lib/buildpackutil.py
+++ lib/buildpackutil.py
@@ -330,7 +330,7 @@ def ensure_and_get_mono(mx_version, cache_dir):
         logging.debug("Mono not found in default locations")
         download_and_unpack(
             #get_blobstore_url("/mx-buildpack/" + mono_version + "-mx.tar.gz"),
-             "https://mxblobstore.azureedge.net/mxblobstore/ + mono_version + "-mx-bionic.tar.gz",
+             "https://mxblobstore.azureedge.net/mxblobstore/" + mono_version + "-mx-bionic.tar.gz",
             os.path.join(fallback_location, mono_version),
             cache_dir,
         )
