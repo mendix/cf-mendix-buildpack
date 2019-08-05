6f7a88da87a0985e9ce8b7f9f36fcef56b620673
Chris Wesdorp <chris.wesdorp@mendix.com>
2019-08-05 10:24:52 +0200

    Introduce TEST_PREFIX env var to change app name
    TEST_PREFIX can be set up to change the 'ops' prefix of apps deployed.
    This can be useful when the target org/space is used my multiple
    developers simultanously.

--- tests/usecase/basetest.py
+++ tests/usecase/basetest.py
@@ -45,7 +45,9 @@ class BaseTest(unittest.TestCase):
         )
         self.mx_password = os.environ.get("MX_PASSWORD", "Y0l0lop13#123")
         self.app_id = str(uuid.uuid4()).split("-")[0]
-        self.subdomain = "ops-" + self.app_id
+
+        self.app_prefix = os.environ.get("TEST_PREFIX", "ops-")
+        self.subdomain = "{}-{}".format(self.app_prefix, self.app_id)
         self.app_name = "%s.%s" % (self.subdomain, self.cf_domain)
 
     def setUp(self):
