c045d7c80b90ce61a8a7507430b37e3e242225d4
Mark Bastiaans <mark.bastiaans@mendix.com>
2020-05-08 13:07:41 +0200

    Added extra cookie header

--- buildpack/nginx.py
+++ buildpack/nginx.py
@@ -39,15 +39,13 @@ def set_up_files(m2ee):
         mxbuild_upstream = "return 501"
     with open("nginx/conf/nginx.conf") as fh:
         lines = "".join(fh.readlines())
-    http_headers = parse_headers()
+
+    samesite_cookie_workaround = get_path_config(
+        MXVersion(str(m2ee.config.get_runtime_version())) < MXVersion("8.10")
+    )
+    http_headers = parse_headers(samesite_cookie_workaround)
     lines = (
-        lines.replace(
-            "CONFIG",
-            get_path_config(
-                MXVersion(str(m2ee.config.get_runtime_version()))
-                < MXVersion("8.10")
-            ),
-        )
+        lines.replace("CONFIG", samesite_cookie_workaround,)
         .replace("NGINX_PORT", str(util.get_nginx_port()))
         .replace("RUNTIME_PORT", str(util.get_runtime_port()))
         .replace("ADMIN_PORT", str(util.get_admin_port()))
@@ -67,7 +65,7 @@ def set_up_files(m2ee):
     )
 
 
-def parse_headers():
+def parse_headers(samesite_cookie_workaround=False):
     header_config = ""
     headers_from_json = {}
 
@@ -103,6 +101,11 @@ def parse_headers():
                     header_key, header_value
                 )
             )
+
+    if samesite_cookie_workaround:
+        header_value = 'add_header Set-Cookie "mx-cookie-test=allowed; SameSite=None; Secure; Path=/" always;\n'
+        escaped_value = header_value.replace('"', '\\"').replace("'", "\\'")
+        header_config += escaped_value
 
     return header_config
 
