a4cd0828cbf67f50cf2c6d6fae7672d26d8e6fea
Aniruddha Galgali <aniruddha.galgali@mendix.com>
2021-04-14 16:13:50 +0530

    Upgrade metering sidecar to support emailDomain

--- buildpack/metering.py
+++ buildpack/metering.py
@@ -7,7 +7,7 @@ from buildpack import util
 from buildpack.runtime_components import database
 
 NAMESPACE = "metering"
-SIDECAR_VERSION = "v0.0.2"
+SIDECAR_VERSION = "v1.0.0"
 SIDECAR_ARCHIVE = "metering-sidecar-linux-amd64-{}.tar.gz".format(
     SIDECAR_VERSION
 )
