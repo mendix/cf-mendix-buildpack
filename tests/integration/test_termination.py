51376e3528d8ddc7e9196ffa28d681bea5b43048
Mark Bastiaans <mark.bastiaans@mendix.com>
2021-05-27 20:34:49 +0200

    Add exit code assertions to shutdown tests

--- tests/integration/test_termination.py
+++ tests/integration/test_termination.py
@@ -3,15 +3,16 @@ from tests.integration import basetest
 
 class TestCaseTermination(basetest.BaseTest):
 
-    # Tests if termination works if the runtime shuts down by itself
-    def test_termination_runtime_intiated(self):
+    # Tests if termination works if a shutdown command is sent to the runtime
+    def test_termination_shutdown_command(self):
         self.stage_container("Mendix8.1.1.58432_StarterApp.mda")
         self.start_container()
         self.assert_app_running()
         self.query_mxadmin({"action": "shutdown"})
         self.assert_string_in_recent_logs("Mendix Runtime is shutting down")
         self.assert_string_in_recent_logs("Mendix Runtime is now shut down")
-        assert self.await_container_status("unhealthy", 60)
+        assert self.await_container_health("unhealthy", 60)
+        assert self.get_container_exitcode() == 0
 
     # Tests if the runtime is shut down gracefully on SIGTERM
     def test_termination_sigterm(self):
@@ -21,7 +22,8 @@ class TestCaseTermination(basetest.BaseTest):
         self.terminate_container()
         self.assert_string_in_recent_logs("Mendix Runtime is shutting down")
         self.assert_string_in_recent_logs("Mendix Runtime is now shut down")
-        assert self.await_container_status("unhealthy"), 60
+        assert self.await_container_health("unhealthy"), 60
+        assert self.get_container_exitcode() == 0
 
     # Tests that the process terminates with a stack trace when Python code
     # errors. The env variable S3_ENCRYPTION_KEYS is used here, it doesn't
@@ -36,6 +38,7 @@ class TestCaseTermination(basetest.BaseTest):
         self.assert_string_in_recent_logs(
             'json.loads(os.getenv("S3_ENCRYPTION_KEYS"))'
         )
+        assert self.get_container_exitcode() == 1
 
     # Tests if a broken application terminates
     def test_termination_broken_application(self):
@@ -43,9 +46,10 @@ class TestCaseTermination(basetest.BaseTest):
             "Sample-StartError-7.23.2.mda",
             env_vars={"METRICS_INTERVAL": "10",},
         )
-        self.start_container(status="unhealthy")
+        self.start_container(health="unhealthy")
         self.assert_string_in_recent_logs("start failed")
         self.assert_string_not_in_recent_logs("health check never passed")
+        assert self.get_container_exitcode() == 1
 
     # Tests if killing Java terminates the container
     def test_termination_java_crash_triggers_unhealthy(self):
@@ -55,4 +59,5 @@ class TestCaseTermination(basetest.BaseTest):
         self.start_container()
         self.assert_app_running()
         self.run_on_container("killall java")
-        assert self.await_container_status("unhealthy", 60)
+        assert self.await_container_health("unhealthy", 60)
+        assert self.get_container_exitcode() == 1
