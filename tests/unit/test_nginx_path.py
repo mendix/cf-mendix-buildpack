904a52c875b45f5a14fddbf1bcd56fb2e3e1e586
Mark Bastiaans <mark.bastiaans@mendix.com>
2021-06-25 13:44:24 +0200

    Disable proxy error interception for dynamic request handlers Extract request handlers from native REST module Swagger templates Various refactoring and improvements

--- tests/unit/test_nginx_path.py
+++ tests/unit/test_nginx_path.py
@@ -1,4 +1,3 @@
-import json
 import os
 import unittest
 
@@ -8,14 +7,14 @@ from buildpack import nginx
 class TestCaseNginxBinPath(unittest.TestCase):
     def test_default_nginx_bin_path(self):
         del os.environ["NGINX_CUSTOM_BIN_PATH"]
-        nginx_bin_path = nginx.get_nginx_bin_path()
-        is_custom_nginx = nginx.is_custom_nginx()
+        nginx_bin_path = nginx._get_nginx_bin_path()
+        is_custom_nginx = nginx._is_custom_nginx()
         self.assertEquals("nginx/sbin/nginx", nginx_bin_path)
         self.assertFalse(is_custom_nginx)
 
     def test_custom_nginx_bin_path(self):
         os.environ["NGINX_CUSTOM_BIN_PATH"] = "/usr/sbin/nginx"
-        nginx_bin_path = nginx.get_nginx_bin_path()
-        is_custom_nginx = nginx.is_custom_nginx()
+        nginx_bin_path = nginx._get_nginx_bin_path()
+        is_custom_nginx = nginx._is_custom_nginx()
         self.assertEquals("/usr/sbin/nginx", nginx_bin_path)
         self.assertTrue(is_custom_nginx)
