5f47763a9b0e97e3669e1f44125151dadccd4d60
Sijo Kolenchery <sijo.pappachan@gmail.com>
2021-11-16 17:18:27 +0530

    For freeapps, configure micrometer to push only session metrics
    We make use of the allow/deny rules to filter out all metrics but sessions
    metrics from being sent for freeapps.

--- tests/unit/test_micrometer_metrics.py
+++ tests/unit/test_micrometer_metrics.py
@@ -1,9 +1,10 @@
 import os
 
 from buildpack.telemetry import metrics
+from lib.m2ee.version import MXVersion
 
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 
 class TestMicrometerMetrics(TestCase):
@@ -33,3 +34,32 @@ class TestMicrometerMetrics(TestCase):
         with patch.dict(os.environ, {"DISABLE_MICROMETER_METRICS": "false"}):
             actual_state = metrics.micrometer_metrics_enabled("9.7.0")
         self.assertTrue(actual_state)
+
+
+class TestMicrometerMetricRegistry(TestCase):
+    def setUp(self) -> None:
+        self.addCleanup(patch.stopall)
+        patch(
+            "buildpack.core.runtime.get_runtime_version",
+            return_value=MXVersion(9.7),
+        ).start()
+        patch(
+            "buildpack.telemetry.metrics.micrometer_metrics_enabled",
+            return_value=True,
+        ).start()
+
+    def test_paidapps_metrics_registry(self):
+        with patch.dict(os.environ, {"PROFILE": "some-random-mx-profile"}):
+            result = metrics.configure_influx_registry(Mock())
+            self.assertEqual(
+                result.get("Metrics.Registries"),
+                metrics.PAIDAPPS_METRICS_REGISTRY,
+            )
+
+    def test_freeapps_metrics_registry(self):
+        with patch.dict(os.environ, {"PROFILE": "free"}):
+            result = metrics.configure_influx_registry(Mock())
+            self.assertEqual(
+                result.get("Metrics.Registries"),
+                metrics.FREEAPPS_METRICS_REGISTRY,
+            )
