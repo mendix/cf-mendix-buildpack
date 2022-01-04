e94926cba745b17d10b6a0dc2b1539095c82a961
Abhishek Amte <abhishek.amte@mendix.com>
2022-01-04 11:32:30 +0100

    Improved Data Broker Business Events error handling of config injection in case service brokers have failures

--- tests/unit/test_databroker_business_events.py
+++ tests/unit/test_databroker_business_events.py
@@ -42,8 +42,32 @@ class TestDataBrokerBusinessEvents(unittest.TestCase):
         }}
     """
 
-    def test_business_events_config(self):
-        os.environ["VCAP_SERVICES"] = self.kafka_vcap
+    kafka_vcap_with_null_creds = f"""
+        {{
+            "kafka-testfree": [
+                {{
+                    "binding_guid": "8ee827cd-d718-446c-b956-224845d7faf4",
+                    "binding_name": null,
+                    "credentials": null,
+                    "instance_guid": "d1564e67-2aeb-4a78-ac7b-db0752de574e",
+                    "instance_name": "3d8fbea3-3741-4a8b-8d9c-5cd189960459-data",
+                    "label": "kafka-testfree",
+                    "name": "3d8fbea3-3741-4a8b-8d9c-5cd189960459-data",
+                    "plan": "shared-business-events-testfree",
+                    "provider": null,
+                    "syslog_drain_url": null,
+                    "tags": [
+                    "databroker",
+                    "MSK",
+                    "Kafka"
+                    ],
+                    "volume_mounts": []
+                }}
+            ]
+        }}
+    """
+
+    def _verify_vcap_info(self):
         business_events_cfg = business_events._get_config(
             util.get_vcap_services_data()
         )
@@ -57,3 +81,14 @@ class TestDataBrokerBusinessEvents(unittest.TestCase):
             business_events_cfg[f"{prefix}.ChannelName"] == self.channel_name
         )
         assert business_events_cfg[f"{prefix}.Username"] == self.username
+
+    def test_business_events_config_happy_flow(self):
+        os.environ["VCAP_SERVICES"] = self.kafka_vcap
+        self._verify_vcap_info()
+
+    def test_business_events_config_with_empty_creds(self):
+        os.environ["VCAP_SERVICES"] = self.kafka_vcap_with_null_creds
+        # make sure any exceptions in the business events does not cause any errors
+        business_events_cfg = business_events._get_config(
+            util.get_vcap_services_data()
+        )
