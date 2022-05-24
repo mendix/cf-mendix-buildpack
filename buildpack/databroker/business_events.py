b3dd990362aeb1fe8067fe0f3903a86b6ec00d17
Abhishek Amte <abhishek.amte@mendix.com>
2022-05-24 14:54:38 +0200

    [DB-1613] Databroker: Business Events fix config map version

--- buildpack/databroker/business_events.py
+++ buildpack/databroker/business_events.py
@@ -11,12 +11,15 @@ import requests
 
 CONSTANTS_PREFIX = "BusinessEvents"
 
-DEDICATED_PLAN_CONSTANTS = [
+BE_CONSTANTS_TO_INJECT = [
     "ServerUrl",
     "UserName",
     "Password",
     "ClientConfiguration",
 ]
+
+OPTIONAL_CONSTANT_APPLY_LIMITS = "ApplyLimits"
+
 CLIENT_CONFIG_URL_KEY = "ClientConfigUrl"
 
 
@@ -24,6 +27,21 @@ def update_config(m2ee, vcap_services_data):
     # if kafka is present in vcap services upsert Business Events constants
     util.upsert_microflow_constants(m2ee, _get_config(vcap_services_data))
     logging.debug("Business Events config added to MicroflowConstants")
+
+
+def _get_client_config(url, auth_token, version):
+    headers = {
+        "Authorization": f"Bearer {auth_token}",
+        "X-Version": version,
+    }
+
+    resp = requests.get(
+        url=url,
+        headers=headers,
+        timeout=30,
+    )
+    resp.raise_for_status()
+    return resp.text
 
 
 def _get_config(vcap_services):
@@ -41,27 +59,30 @@ def _get_config(vcap_services):
                     and service_creds[0]["credentials"]
                 ):
                     kafka_creds = service_creds[0]["credentials"]
-                    if "ChannelName" in kafka_creds:
-                        for key, value in kafka_creds.items():
-                            be_config[f"{CONSTANTS_PREFIX}.{key}"] = value
-                    elif CLIENT_CONFIG_URL_KEY in kafka_creds:
+                    if CLIENT_CONFIG_URL_KEY in kafka_creds:
                         auth_token = kafka_creds.get(
                             "ClientConfigAuthToken", ""
                         )
-                        headers = {"Authorization": f"Bearer {auth_token}"}
-                        resp = requests.get(
-                            url=kafka_creds.get(CLIENT_CONFIG_URL_KEY, ""),
-                            headers=headers,
-                            timeout=30,
-                        )
-                        resp.raise_for_status()
-                        for constant in DEDICATED_PLAN_CONSTANTS:
+                        for constant in BE_CONSTANTS_TO_INJECT:
                             be_config[
                                 f"{CONSTANTS_PREFIX}.{constant}"
                             ] = kafka_creds.get(constant, "")
+                        client_config = _get_client_config(
+                            kafka_creds.get(CLIENT_CONFIG_URL_KEY, ""),
+                            auth_token,
+                            "1",
+                        )
                         be_config[
                             f"{CONSTANTS_PREFIX}.ClientConfiguration"
-                        ] = resp.text
+                        ] = client_config
+                        if kafka_creds.get(OPTIONAL_CONSTANT_APPLY_LIMITS):
+                            be_config[
+                                f"{CONSTANTS_PREFIX}.{OPTIONAL_CONSTANT_APPLY_LIMITS}"
+                            ] = kafka_creds.get(OPTIONAL_CONSTANT_APPLY_LIMITS)
+                    else:
+                        logging.error(
+                            "Business Events: configuration is not compatible, please unbind and bind the service again"
+                        )
                 else:
                     logging.error("Business Events: configuration is empty")
     except Exception as ex:
--- tests/unit/test_databroker_business_events.py
+++ tests/unit/test_databroker_business_events.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from unittest import mock
 
 from buildpack.databroker import business_events
 from buildpack import util
@@ -9,9 +10,12 @@ class TestDataBrokerBusinessEvents(unittest.TestCase):
     server_url = "b-3.mendix.kafka.eu-west-1.amazonaws.com:9096,b-1.mendix.kafka.eu-west-1.amazonaws.com:9096,b-2.mendix.kafka.eu-west-1.amazonaws.com:9096"
     apply_limits = "true"
     password = "abc23e4ftas78"
-    channel_name = "e3c890204da74768"
     username = "ZaUx5uboxqsCRrA"
-    kafka_shared_vcap = f"""
+    client_config_token = "adsads45678idassdasdasdaafXK6q7R"
+    client_config_url = (
+        "https://testconfig.mendix.com/client-configs/AEPbRQqHCb6"
+    )
+    kafka_shared_vcap_free = f"""
         {{
             "kafka-testfree": [
                 {{
@@ -21,8 +25,40 @@ class TestDataBrokerBusinessEvents(unittest.TestCase):
                         "ServerUrl": "{server_url}",
                         "ApplyLimits": "{apply_limits}",
                         "Password": "{password}",
-                        "ChannelName": "{channel_name}",
-                        "Username": "{username}"
+                        "UserName": "{username}",
+                        "ClientConfigAuthToken": "{client_config_token}",
+                        "ClientConfigUrl": "{client_config_url}"
+                    }},
+                    "instance_guid": "d1564e67-2aeb-4a78-ac7b-db0752de574e",
+                    "instance_name": "3d8fbea3-3741-4a8b-8d9c-5cd189960459-data",
+                    "label": "kafka-testfree",
+                    "name": "3d8fbea3-3741-4a8b-8d9c-5cd189960459-data",
+                    "plan": "shared-business-events-testfree",
+                    "provider": null,
+                    "syslog_drain_url": null,
+                    "tags": [
+                    "databroker",
+                    "MSK",
+                    "Kafka"
+                    ],
+                    "volume_mounts": []
+                }}
+            ]
+        }}
+    """
+
+    kafka_shared_vcap_licensed = f"""
+        {{
+            "kafka-testlicensed": [
+                {{
+                    "binding_guid": "8ee827cd-d718-446c-b956-224845d7faf4",
+                    "binding_name": null,
+                    "credentials": {{
+                        "ServerUrl": "{server_url}",
+                        "Password": "{password}",
+                        "UserName": "{username}",
+                        "ClientConfigAuthToken": "{client_config_token}",
+                        "ClientConfigUrl": "{client_config_url}"
                     }},
                     "instance_guid": "d1564e67-2aeb-4a78-ac7b-db0752de574e",
                     "instance_name": "3d8fbea3-3741-4a8b-8d9c-5cd189960459-data",
@@ -67,24 +103,71 @@ class TestDataBrokerBusinessEvents(unittest.TestCase):
         }}
     """
 
-    def _verify_vcap_info(self):
-        business_events_cfg = business_events._get_config(
-            util.get_vcap_services_data()
-        )
-        prefix = business_events.CONSTANTS_PREFIX
-        assert business_events_cfg[f"{prefix}.ServerUrl"] == self.server_url
-        assert (
-            business_events_cfg[f"{prefix}.ApplyLimits"] == self.apply_limits
-        )
-        assert business_events_cfg[f"{prefix}.Password"] == self.password
-        assert (
-            business_events_cfg[f"{prefix}.ChannelName"] == self.channel_name
-        )
-        assert business_events_cfg[f"{prefix}.Username"] == self.username
+    expected_client_config = """
+        {
+            "version": "1.0.0",
+            "allowedOverrides": [
+                "EventNameEnvironmentMap"
+            ],
+            "publish": {
+                "events": [
+                {
+                    "eventName": "OrderAccepted",
+                    "channel": "169b7967830f400381d54196fffe879d",
+                    "topic": "businessevents.169b7967830f400381d54196fffe879d.1307defb000a43b1a7b5b1258587d14b"
+                }
+                ]
+            },
+            "subscribe": {
+                "clientConfig": {
+                "consumerGroupIdPrefix": "1307defb-000a-43b1-a7b5-b1258587d14b"
+                },
+                "events": [
+                {
+                    "eventName": "OrderShipped",
+                    "channel": "0779c0d0269349ee9a3728eb56106bae",
+                    "partialTopic": "businessevents.0779c0d0269349ee9a3728eb56106bae"
+                },
+                {
+                    "eventName": "InventoryUpdate",
+                    "channel": "509d8fd738da49358a6fb0b0b108d8eb",
+                    "partialTopic": "businessevents.509d8fd738da49358a6fb0b0b108d8eb"
+                }
+                ]
+            }
+        }
+    """
 
-    def test_business_events_config_happy_flow(self):
-        os.environ["VCAP_SERVICES"] = self.kafka_shared_vcap
+    def _verify_vcap_info(self, is_apply_limits_present=True):
+        with mock.patch(
+            "buildpack.databroker.business_events._get_client_config",
+            mock.MagicMock(return_value=self.expected_client_config),
+        ):
+            business_events_cfg = business_events._get_config(
+                util.get_vcap_services_data()
+            )
+        prefix = business_events.CONSTANTS_PREFIX
+
+        assert business_events_cfg[f"{prefix}.ServerUrl"] == self.server_url
+        assert business_events_cfg[f"{prefix}.Password"] == self.password
+        assert business_events_cfg[f"{prefix}.UserName"] == self.username
+        assert (
+            business_events_cfg[f"{prefix}.ClientConfiguration"]
+            == self.expected_client_config
+        )
+        if is_apply_limits_present:
+            assert (
+                business_events_cfg[f"{prefix}.ApplyLimits"]
+                == self.apply_limits
+            )
+
+    def test_business_events_config_happy_flow_free(self):
+        os.environ["VCAP_SERVICES"] = self.kafka_shared_vcap_free
         self._verify_vcap_info()
+
+    def test_business_events_config_happy_flow_licensed(self):
+        os.environ["VCAP_SERVICES"] = self.kafka_shared_vcap_licensed
+        self._verify_vcap_info(is_apply_limits_present=False)
 
     def test_business_events_config_with_empty_creds(self):
         os.environ["VCAP_SERVICES"] = self.kafka_shared_vcap_with_null_creds
