1c826fa916a688ad6e14a9d13078b3eb5d9003c4
denis-shuvalov <74789092+denis-shuvalov@users.noreply.github.com>
2022-11-10 11:43:24 +0100

    Periodic submits of 'jvm.errors', and 'jvm.ooms' metrics added; 'jvm.crash' metric removed. (#580)
    * Intervally updating jvm.errors and jvm.ooms metrics
    
    * Remove jvm.crach metric
    
    Co-authored-by: Denis Shuvalov <denis.shuvalov@mendix.com>

--- buildpack/telemetry/metrics.py
+++ buildpack/telemetry/metrics.py
@@ -1,4 +1,3 @@
-import atexit
 import datetime
 import json
 import logging
@@ -80,10 +79,6 @@ METRICS_REGISTRIES_KEY = "Metrics.Registries"
 # from the micrometer library via the telegraf agent
 MXVERSION_MICROMETER = MXVersion("9.7.0")
 
-# Handler for exit(0) and exit(1)
-def _stop():
-    _emit(jvm={"crash": 1.0})
-
 
 # Handler for user signals
 # Initialized from within the start procedure in start.py
@@ -113,7 +108,6 @@ def int_or_default(value, default=0):
 def run(m2ee):
     metrics_interval = os.getenv("METRICS_INTERVAL")
     if metrics_interval:
-        atexit.register(_stop)
         if util.is_free_app():
             thread = FreeAppsMetricsEmitterThread(int(metrics_interval), m2ee)
         else:
@@ -284,6 +278,7 @@ class BaseMetricsEmitterThread(threading.Thread, metaclass=ABCMeta):
         self.micrometer_metrics_enabled = micrometer_metrics_enabled(
             runtime.get_runtime_version()
         )
+
         if bypass_loggregator():
             logging.info("Metrics are logged direct to metrics server.")
             self.emitter = MetricsServerEmitter(metrics_url=get_metrics_url())
@@ -442,6 +437,16 @@ class BaseMetricsEmitterThread(threading.Thread, metaclass=ABCMeta):
             stats["mendix_runtime"][
                 "critical_logs_count"
             ] = critical_logs_count
+        return stats
+
+    def _inject_jvm_failure_metrics(self, stats):
+
+        if "jvm" not in stats:
+            stats["jvm"] = {}
+
+        stats["jvm"]["errors"] = 0.
+        stats["jvm"]["ooms"] = 0.
+
         return stats
 
     def _inject_storage_stats(self, stats):
@@ -681,6 +686,7 @@ class PaidAppsMetricsEmitterThread(BaseMetricsEmitterThread):
         else:
             selected_stats.append(self._inject_smap_stats)
         selected_stats.append(self._inject_critical_log_stats)
+        selected_stats.append(self._inject_jvm_failure_metrics)
 
         return selected_stats
 
