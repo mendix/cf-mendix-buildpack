404f90ae26201b996d6912c82d5599f15568935a
Gaurav Shah <gaurav.shah@mendix.com>
2023-11-28 19:25:26 +0530

    updated log level

--- buildpack/telemetry/metrics.py
+++ buildpack/telemetry/metrics.py
@@ -111,12 +111,12 @@ def micrometer_metrics_enabled(runtime_version):
     logging.info("checking is micrometer metrics enabled")
     micrometer_enabled=False
     if(_micrometer_runtime_requirement(runtime_version)):
-        logging.info("satisfies micrometer runtime requirement")
+        logging.debug("satisfies micrometer runtime requirement")
         if(bool(get_micrometer_metrics_url())):
-            logging.info("Found micrometer metrics url configured")
+            logging.debug("Found micrometer metrics url configured")
             micrometer_enabled = True
         elif(strtobool(os.getenv("NON_MENDIX_PUBLIC_CLOUD","false"))):
-            logging.info("micrometer for non mendix public cloud")
+            logging.debug("micrometer for non mendix public cloud")
             micrometer_enabled =  True
     return micrometer_enabled
     
