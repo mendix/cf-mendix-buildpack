5881bd4198e817074463e032bc24278a2959678c
Mukund Padale <mukund.padale@mendix.com>
2024-01-24 12:11:07 +0530

    added exception handling if key is not found

--- buildpack/core/runtime.py
+++ buildpack/core/runtime.py
@@ -104,7 +104,7 @@ def get_metadata_value(key, build_path=BASE_PATH):
         with open(file_name) as file_handle:
             data = json.loads(file_handle.read())
             return data[key]
-    except IOError:
+    except (IOError, KeyError):
         return None
 
 
