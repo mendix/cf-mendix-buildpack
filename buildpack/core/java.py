61d0b2d2d9b4c6fed11a812285c47de5c06b92f6
Mukund Padale <mukund.padale@mendix.com>
2024-01-29 16:53:50 +0530

    added log line for debug - 1

--- buildpack/core/java.py
+++ buildpack/core/java.py
@@ -21,7 +21,7 @@ def get_java_major_version(runtime_version):
         return _get_major_version(os.getenv(JAVA_VERSION_OVERRIDE_KEY))
     if runtime_version >= MXVersion("8.0.0"):
         result = runtime.get_metadata_value("JavaVersion")
-        logging.info("get_java_major_version : % ", runtime_version)
+        logging.info("get_java_major_version : %s ", str(runtime_version))
         if result is None:
             logging.info("get_java_major_version - None")
             result = 11 # default version for mx 8 or above
