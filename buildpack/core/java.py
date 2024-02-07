2fe679dab13cf592a01a62c7c400d697075055cb
Mukund Padale <mukund.padale@mendix.com>
2024-02-07 16:14:45 +0530

    removed extra logs

--- buildpack/core/java.py
+++ buildpack/core/java.py
@@ -19,22 +19,16 @@ SUPPORTED_GC_COLLECTORS = ["Serial", "G1"]
 def get_java_major_version(runtime_version, build_path=BASE_PATH):
     result = 8
     if os.getenv(JAVA_VERSION_OVERRIDE_KEY):
-        logging.info("get_java_major_version - 1")
         return _get_major_version(os.getenv(JAVA_VERSION_OVERRIDE_KEY))
     if runtime_version >= MXVersion("8.0.0"):
         result = runtime.get_metadata_value("JavaVersion", build_path)
-        logging.info("get_java_major_version : %s ", str(runtime_version))
         if result is None:
-            logging.info("get_java_major_version - None")
             result = 11 # default version for mx 8 or above
         elif "17" in result:
-            logging.info("get_java_major_version - 17")
             result = 17
         elif "21" in result:
-            logging.info("get_java_major_version - 21")
             result = 21
         else:
-            logging.info("get_java_major_version - 11")
             result = 11
     major_version = _get_major_version(result)
     logging.info("get_java_major_version : major_version - %s", str(major_version))
