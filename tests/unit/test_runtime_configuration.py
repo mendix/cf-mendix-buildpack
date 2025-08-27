991aec5b46844307f53bb98aaa153a110f3cb521
Sanny Ramirez <sanny.ramirez@mendix.com>
2025-08-27 10:38:49 +0200

    DEPS-436 fixed unit test case for runtime configuration

--- tests/unit/test_runtime_configuration.py
+++ tests/unit/test_runtime_configuration.py
@@ -12,8 +12,8 @@ from cryptography.hazmat.primitives.serialization import pkcs12
 from cryptography.x509.oid import NameOID
 
 from buildpack import util
-from buildpack.core import runtime#, security
-#from lib.m2ee.version import MXVersion
+from buildpack.core import runtime, security
+from lib.m2ee.version import MXVersion
 
 
 class M2EEMock:
@@ -149,12 +149,12 @@ class TestClientCertificateConfiguration(TestCase):
         )
     }
 
-#    @mock.patch.dict(os.environ, CERTIFICATE_ENV)
-#    def test_selfsigned_certificate_less_mx720(self):
-#        result = security.get_client_certificates(MXVersion(7.16))
-#        assert "WebServiceClientCertificates" in result
+    @mock.patch.dict(os.environ, CERTIFICATE_ENV)
+    def test_selfsigned_certificate_less_mx720(self):
+        result = security.get_client_certificates(MXVersion(7.16))
+        assert "WebServiceClientCertificates" in result
 
-#    @mock.patch.dict(os.environ, CERTIFICATE_ENV)
-#    def test_selfsigned_certificate_greq_mx720(self):
-#        result = security.get_client_certificates(MXVersion(7.23))
-#        assert "ClientCertificateUsages" in result
+    @mock.patch.dict(os.environ, CERTIFICATE_ENV)
+    def test_selfsigned_certificate_greq_mx720(self):
+        result = security.get_client_certificates(MXVersion(7.23))
+        assert "ClientCertificateUsages" in result
